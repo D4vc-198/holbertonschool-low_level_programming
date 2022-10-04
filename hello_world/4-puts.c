515bb33f850b0322d421e87381ab55b38efe49ed
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-04 00:03:27 -0500

    4.Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * pust return string
+ * Return Always 0 (0)
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
