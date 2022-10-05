53adb30a5a35ae6b4554a4ffe54f349e44c6dd54
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-05 14:43:26 -0500

    2.I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,12 @@
 */
 int main(void)
 {
-    char character;
-    for (character = 'a'; character <= 'z'; character++)
-    {
-        putchar(character);
-    }
-    return (0);
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		putchar(character);
+	}
+	putchar('\n');
+	return (0);
 }
