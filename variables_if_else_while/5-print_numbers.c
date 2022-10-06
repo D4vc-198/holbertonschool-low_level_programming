2f96fa75958c4f4ef8882dfbe10bf0bbfc604427
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-05 23:30:44 -0500

    5.Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entryes point
+ *
+ * Return: always 0 (Succes)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+	return (0);
+}
