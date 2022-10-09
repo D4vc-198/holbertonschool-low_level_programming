97fd2ddfd198a274da0882e0c5dbca71144b9251
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-08 21:45:15 -0500

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,14 @@
 char *test (void);
+
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+    char character;
+
+    for (character = 'a'; character <= 'z'; character++)
+    {
+        _putchar(character);
+    }
+    _putchar('\n');
+}
