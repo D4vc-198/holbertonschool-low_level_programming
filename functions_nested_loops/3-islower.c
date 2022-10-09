0f6471be2bddb92b36048b110917934f99676158
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-08 23:11:16 -0500

    loer

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isLower return 1
+ *
+ * other case return 0
+*/
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
