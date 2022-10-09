ec7d8ceec5dbe2f2f06429999069373c14eb1107
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-09 07:52:37 -0500

    5.Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,21 +2,25 @@
 
 int print_sign(int n)
 {
-	char *valor;
+	int valor;
+	char simbol;
 
 	if(n > 0)
 	{
-		*valor = "+1";
+		valor = 1;
+		putchar("+");
+		return (valor);
 	}
 	if (n == 0)
 	{
-		*valor = "0";
+		valor = 0;
+		return (valor);
 	}
 	else 
 	{
-		*valor = "-1";
+		valor = 1;
+		putchar("-");
+		return (valor);
 	}
-
-	return valor;
 
 }
