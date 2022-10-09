0ea55fd0fe6d6123c98aa9a982196c3fd528183f
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-08 22:55:01 -0500

    2.10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return: Alphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	int contador = 0;
+
+	while (contador <= 9)
+	{
+		char alphabet;
+
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			putchar(alphabet);
+		}
+		contador++;
+	}
+}
