c447f17aff441a2d83e73922f2476d9827cb42dd
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-09 22:46:02 -0500

    5.Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,21 +2,22 @@
 
 int print_sign(int n)
 {
-	int valor;
+	signed int valor;
 	
 	if(n > 0)
 	{
 		valor = 1;
-		putchar('+');
+		return printf("%+d", valor);
 	}
 	if (n == 0)
 	{
 		valor = 0;
+		return printf("%d", valor);
 	}
 	if (n < 0) 
 	{
 		valor = 1;
+		return printf("%-d", valor);
 	}
 
-	return (valor);
 }
---
+++ functions_nested_loops/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int valor;
+    
+    valor = +1;
+    return printf("%+d", valor);
+    
+}
