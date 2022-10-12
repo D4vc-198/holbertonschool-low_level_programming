5a8369ad95161b56017299be8e8e0129c19f92a0
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-11 21:54:05 -0500

    6.There is no such thing as absolute value in this world.

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
-
+/**
+ * _abs - return absolute value number
+ * @num: is a number param
+ * Return: valorAbs - num
+ */
 int _abs(int num)
 {
 	int valorAbs = abs(num);
+
 	return (valorAbs);
 }
