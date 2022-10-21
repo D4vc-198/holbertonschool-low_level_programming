844faba2d469a7314931b9ff939729a71ee3cd52
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-21 13:04:01 -0500

    4.I believe in numbers and signs

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 01356789
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <=9; number++)
+	{
+		if (number != 2 && number != 4)
+		{
+			_putchar('0' + number);
+		}
+	}
+
+	_putchar('\n');
+}
