2482011a650b1098fe456b17facea646f13ca768
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-21 12:53:44 -0500

    3. The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+
 void print_numbers(void)
 {
 	int number;
 
 	for (number = 0; number <= 9; number ++)
 	{
-		_putchar(number);
+		_putchar('0' + number);
 	}
-	_putchar("\n");
+
+	_putchar('\n');
 
 }
