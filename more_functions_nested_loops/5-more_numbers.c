6648253ee515e1b014c12359573bf34fd8a53c13
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-21 13:31:47 -0500

    5.Numbers constitute the only universal languaje

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,21 +3,17 @@
 void more_numbers(void)
 {
 	int number;
-	int count;
+	int count, i;
 
-	while (count <= 9)
+	for (int i = 0; i <= 10; i++)
 	{
 		for (number = 0; number <= 14; number++)
 		{
-			if(number >= 10)
+			if (number >= 10)
 			{
 				_putchar('0' + number / 10);
 			}
-
 			_putchar('0' + number % 10);
 		}
 		_putchar('\n');
-		count++;
 	}
-
-}
