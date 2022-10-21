96870a1a16cfe0f063733180b5b60d7f07660c6b
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-20 23:21:20 -0500

    0.isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,17 @@
 #include <ctype.h>
-/**
- * _isupper - entryes point
- *@c: is a param
- * Return: valor 
- */
+
 int _isupper(int c)
 {
 	int valor;
-	
-	if (isupper(c))
-		valor = 1;
-	
-	valor = 0;
-	
 
+	if (isupper(c))
+	{
+		valor = 1;
+	}
+	else
+	{
+		valor = 0;
+	}
 	return (valor);
 }
+
