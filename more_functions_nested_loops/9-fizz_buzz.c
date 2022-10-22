495456ddf4903c8c55dd9a6ca10c589ff6c59655
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-22 13:03:06 -0500

    9.Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int numbers;
+	int mult3;
+
+	for (numbers = 1; numbers <= 100; numbers++)
+	{
+		if (numbers % 3 == 0)
+		{
+			if (numbers % 15 == 0)
+				printf("FizzBuzz ");
+
+			printf("Fizz ");
+		}
+
+		 else if ( numbers % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else 
+		{
+			printf("%d ", numbers);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
