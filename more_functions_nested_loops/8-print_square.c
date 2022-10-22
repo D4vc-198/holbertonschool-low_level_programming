c8b1760c1cb3a3185dedbabda005c4cac1ce301c
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-21 22:47:29 -0500

    8.Your are so much sunshine in every square inch

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,18 +1,29 @@
 #include "main.h"
-
+/**
+ * print_square - print square #
+ * @size: is a size square
+ */
 void print_square(int size)
 {
 	int count;
 	int i;
 
 	count = 0;
-	while (count < size)
+
+	if (size <= 0)
 	{
-		for (i = 0; i < size; i++)
+		_putchar('\n')
+	}
+	else
+	{
+		while (count < size)
 		{
-			_putchar('#');
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			count++;
 		}
-		_putchar('\n');
-		count++;
 	}
 }
