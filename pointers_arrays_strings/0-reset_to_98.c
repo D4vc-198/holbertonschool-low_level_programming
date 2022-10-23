69832bd4d022c942640605b17c471f234dab2845
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-23 15:01:50 -0500

    0.98 Battery st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value to 98
+ * reset_to_98 - takes a pointer and update the value to 98
  * @n: pointer
  */
 void reset_to_98(int *n)
