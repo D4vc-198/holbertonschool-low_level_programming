36139caedf145eea7d6f3fe1da67628341e0c1ea
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-26 21:40:33 -0500

    4.I can only go one way. I've not got a reverse gear

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int i;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		if(s[i] == ' ')
+		{
+			s[i] = '\0';
+			printf("%s ", &(s[i]) + 1);
+		}
+	}
+
+	printf("%s", s);
+}
