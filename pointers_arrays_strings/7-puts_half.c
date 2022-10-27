1dd807669185e8c1d9c804606fc5a64a697ea02a
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-27 13:34:22 -0500

    7.Winning is only half of it. Having fun is the other half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -23,7 +23,6 @@ void puts_half(char *str)
 	}
 
 	if (size != 0) {
-		contador = contador - 1;
 		for ( contador = (contador - 1) / 2; str[contador] != '\0'; contador++)
 		{
 			_putchar(str[contador]);
