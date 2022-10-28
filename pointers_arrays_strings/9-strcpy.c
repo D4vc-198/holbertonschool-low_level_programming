2769a9c4841588e3640fa6db742ec428df133cc1
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-27 22:24:38 -0500

    9.strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ * @dest: dest to string
+ * @src: source string
+ * Return: dest
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 	int contador;
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,3 +5,5 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
