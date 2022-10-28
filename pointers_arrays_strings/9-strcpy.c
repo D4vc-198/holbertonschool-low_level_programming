a93012139a95bccb8e92f731528ca486969e58c5
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-27 22:17:36 -0500

    9.strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+	int contador;
+
+	for (contador = 0; *src[contador] != '\0'; contador++)
+	{
+		dest[contador] = *(src + contador);
+	}
+
+	dest[contador] = '\0';
+
+	return (dest);
+}
