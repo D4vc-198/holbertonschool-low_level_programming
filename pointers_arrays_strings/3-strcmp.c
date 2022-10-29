56af574d04ea08e62e6f4a950f67879ba3b87f3b
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-29 12:42:30 -0500

    3.strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	while (s1)
+	{
+		if (s1 != s2)
+		{
+			break;
+		}
+
+		s1++;
+		s2++;
+	}
+
+	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
