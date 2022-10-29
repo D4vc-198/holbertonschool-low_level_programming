fd01ca498ad7d93b1614651304b13e656ffb8c4e
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-28 23:10:18 -0500

    2.strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,9 +4,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int contador;
 
-	for (contador = 0; *(src + contador) != '\0' && contador <= n; contador++)
+	for (contador = 0; src[contador] != '\0' && contador < n; contador++)
 	{
-		dest[contador] = *(src + contador);
+		dest[contador] = src[contador];
 	}
 
 	dest[contador] = '\0';
