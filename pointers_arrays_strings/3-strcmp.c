02d8362fc7c591660dc5ee531f7e0a1c3d5db871
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-29 12:50:45 -0500

    3.strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: int1
+ */
 
 int _strcmp(char *s1, char *s2)
 {
@@ -13,5 +19,5 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 
-	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
 }
