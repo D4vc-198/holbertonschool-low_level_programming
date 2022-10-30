5239dda8b6205e63c26a886d8b5a9769215c940e
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-30 15:33:38 -0600

    8.rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string a using rot13
+ * @str: string to encode
+ * Return: str encode ro13
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char cifrado[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; aplh[j] != '\0'; j++)
+		{
+			if (str[i] == alph[j])
+			{
+				str[i] = cifrado[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
