8d1cfeb7176b0b8f302c6bb3e3219f36f2c24472
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-31 12:55:40 -0600

    0.memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to instert constant
+ * @b: constant
+ * @n: bytes
+ * Return: pointer to the memory area S
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
