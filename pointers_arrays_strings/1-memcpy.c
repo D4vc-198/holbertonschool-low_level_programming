4b8e14f1bf820e730e1e71cb577a63311e6adc50
d4vc-198 <contactodiegoavc@gmail.com>
2022-10-31 13:10:44 -0600

    1.memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memcpy - copies memory area.
+ * @dest: pointer to copies src
+ * @src: source char
+ * int: num bytes
+ * Return: pointer to dest
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
