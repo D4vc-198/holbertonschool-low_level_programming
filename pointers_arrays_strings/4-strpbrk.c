0cdfa54b757e4a6fa02864f6e3e4db5c6579b18e
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-01 22:44:55 -0600

    3.strspn

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: string to search
+ * @accept: matches
+ * Return: first ocurrence
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				s = &s[i];
+				return (s);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
