e673868d80b5c190cf6e56533b33349b5be78339
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-02 15:46:40 -0600

    6. Chess is mental torture

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array 2d
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int column, row;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
