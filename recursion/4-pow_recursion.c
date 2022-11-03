513e95f4f4137fd485fa926940e0c55e5806e6cd
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-02 22:48:32 -0600

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - return value of x
+ * @x: base
+ * @y: exponent
+ * Return: x raised to the power of y
+ */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (_pow_recursion(x, y -1) *x);
+	return (_pow_recursion(x, y - 1) * x);
 }
