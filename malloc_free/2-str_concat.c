72d0e098ec321f4c61622770f3298db638703c1a
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-04 16:26:28 -0600

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
-	concat_str = malloc(sizeof(char) * len);
+	str_i = malloc(sizeof(char) * len);
 
 	if (concat_str == NULL)
 		return (NULL);
