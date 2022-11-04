5949beeaa0ba3da0ce4cad2a3239ffde5192375a
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-03 22:41:50 -0600

    3. Neither irony nor sarcasm is argument

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -4,11 +4,13 @@ int main(int argc, char *argv[])
 {
 
 	if (argc == 0)
-		printf("Error");
+	{
+		printf("Error\n");
+		return (1);
+	}
 	else 
 	{
-		printf("%d", (int)(argv[1] * argv[2]));
+		printf("%d", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-
-	return (0);
 }
