bd6d9a1f65a54ac0240f02d12c5440c33c92d760
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-04 22:06:26 -0600

    6. I will show you how great I am

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -48,7 +48,7 @@ char **strtow(char *str)
 		{
 			if (str[c] == ' ')
 				a1++;
-			if (str[c] != &&(str[c + 1] == ' ' || str[c + 1] == '\0'))
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
