4f3b75720a56c75c10a5145f9dca6593f89cfc4b
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-06 15:07:27 -0600

    5. We must accept finite disappointment, but never lose infinite hope

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * digit - check string contains a non-digit char
  * @str: string
- * Return 0 a non-digit or 1 otherwise
+ * Return: 0 a non-digit or 1 otherwise
  */
 int digit(char *str)
 {
@@ -19,45 +18,49 @@ int digit(char *str)
 	}
 	return (1);
 }
-
 /**
  * _strlen - return lenght of a string
  * @str: string
  * Return: lenght of a string
  */
-
 int _strlen(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
-		i++;
+	{
+		i++
+	}
 	return (i);
+}
+/**
+ * errors - errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
 }
 
 /**
  * main - mult two positive numbers
- * @argc; number of arguments
+ * @argc: number of arguments
  * @argv: array of arguments
  * Return: alway 0 (Success)
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len, len1,len2;
+	int len, len1, len2;
 	int i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !digit(s1) || !digit(s2))
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
@@ -66,7 +69,6 @@ int main (int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
