f0c657ca287f5f35296334252848698aec14121a
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-06 12:55:34 -0600

    0.Trust no one

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory size
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *memory;
+
+	memory = malloc(b);
+	if (memory == NULL)
+		exit(98);
+	return (memory);
+}
