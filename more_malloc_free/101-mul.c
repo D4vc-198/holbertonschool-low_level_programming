94f97c5b826b7b8da451a72d99e2c4bcbf0f3afb
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-06 14:27:59 -0600

    5. We must accept finite disappointment, but never lose infinite hope

---
+++ more_malloc_free/101-mul.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * digit - check string contains a non-digit char
+ * @str: string
+ * Return 0 a non-digit or 1 otherwise
+ */
+int digit(char *str)
+{
+	int i = 0;
+
+	while (str[])
+	{
+		if (string[i] < '0' || string[i] > '9')
+			return (0);
+		i++
+	}
+	return (1);
+}
+
+/**
+ * _strlen - return lenght of a string
+ * @str: string
+ * Return: lenght of a string
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * main - mult two positive numbers
+ * @argc; number of arguments
+ * @argv: array of arguments
+ * Return: alway 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	char *s1, *s2;
+	int lenght, lenght1,lenght2;
+	int i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	{
+		pirntf("Error\n");
+		exit(98);
+	}
+	lenght1 = _strlen(s1);
+	lenght2 = _strlen(s2);
+	lenght = lenght1 + lenght2 + 1;
+	result = malloc(sizeof(int) * lenght);
+
+	if (!result)
+		return (1);
+	for (i = 0; i < lenght1 + lenght2; i++)
+		result[i] = 0;
+	for (lenght1 = lenght1 - 1; lenght1 >= 0; lengh1--)
+	{
+		digit1 = s1[leght1] - '0';
+		carry = 0;
+
+		for (lenght2 = _strlen(s2) - 1; lenght2 >= 0; lenght2--)
+		{
+			digit2 = s2[lenght2] - '0';
+			carrt += result[lenght1 + lenght2 + 1] + (digit1 * digit2);
+			result[lenght1 + lenght2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[lenght1 + lenght2 + 1] += carry;
+	}
+	for (i = 0; i < lenght - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
+}
