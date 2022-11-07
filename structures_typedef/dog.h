7bf23d683cc8582be9b842ccd1f8785e52ee3900
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-07 11:55:44 -0600

    0.Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_FILE
 #define DOG_FILE
 
-struct dog 
+/**
+ * struct dog - struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
 {
 	char *name;
 	float age;
