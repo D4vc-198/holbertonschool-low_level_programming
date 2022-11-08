d3bb67a8ce8224ef74b6e5bacdc5ecab2374b5fa
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-08 15:30:22 -0600

    .

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - return sum
+ * @n: integers to add
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int sum = 0, i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
