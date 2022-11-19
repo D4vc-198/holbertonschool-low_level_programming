bf9698331d2c1feed2907142a7d3d65675f0dfff
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-18 23:17:40 -0600

    main.h update

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,9 +1,11 @@
 /**
+ * set_bit - sets the value of a bit
  * @n: pointer to a number
- * @index:
+ * @index: index
  * Return: 1 (Sucess) else -1
  */
- int set_bit(unsigned long int *n, unsigned int index)
+
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64)
 		return (-1);
