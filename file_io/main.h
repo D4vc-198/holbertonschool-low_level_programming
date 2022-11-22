6d3d737b75ada195f38c63280700dd988158ec64
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-22 13:51:29 -0600

    main.h update

---
+++ file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
