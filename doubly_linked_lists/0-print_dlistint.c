ba8e61e7fea5089c03e1b3ff1ebbbdb48a26546e
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-25 22:05:30 -0600

    0.Print list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all data of doubly linked list
+ * @h: pointer to head of list
+ * Return: number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node += 1;
+	}
+
+	return (node);
+}
