9735a095d6934c95af1190201463966f24a3a544
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-03 12:55:48 -0600

    1. Binary search

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,70 @@
+#include "search_algos.h"
+/**
+ * print_array - prints array
+ * @array: sorted array of ints
+ * @high: upper bound
+ * @low: lower bound
+ */
+void print_array(int *array, size_t high, size_t low)
+{
+	if (low > high)
+		return;
+	printf("Searching in array: ");
+	if (low == high)
+	{
+		printf("%u\n", array[high]);
+		return;
+	}
+
+	while (low <= high)
+	{
+		printf("%u", array[low]);
+		if (low != high)
+			printf(", ");
+		low++;
+	}
+	printf("\n");
+}
+/**
+ * b_search - binary search helper function
+ * Recursively searches for value based on midpoint
+ * @array: sorted array of ints
+ * @high: top end of array
+ * @low: lower bound of array
+ * @value: value to search for
+ * Return: postion or -1 if not found
+ */
+int b_search(int *array, size_t high, size_t low, int value)
+{
+	int mid = low + (high - low) / 2;
+
+	print_array(array, high, low);
+
+	if (array[mid] == value)
+		return (mid);
+	if (high >= low)
+	{
+		if (array[mid] > value)
+			return (b_search(array, mid - 1, low, value));
+		else
+			return (b_search(array, high, mid + 1, value));
+	}
+	return (-1);
+}
+/**
+ * binary_search - binary search algorithm
+ * @array: array of sorted ints
+ * @size: size of array
+ * @value: value to serach for
+ * Return: position or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int ret;
+
+	if (array == NULL)
+		return (-1);
+
+	ret = b_search(array, (int)size - 1, 0, value);
+	return (ret);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -37,4 +37,5 @@ typedef struct listint_s
 } listint_t;
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
