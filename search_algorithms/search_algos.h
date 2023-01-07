cfe3e4cf69a8b5f9d184d537228365f91b72e323
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-06 21:41:15 -0600

    13. Linear search in a skip list

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -44,4 +44,10 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
+void free_skiplist(skiplist_t *list);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
